281efff0cc1e01cf8b3171291b460526d720aae9
olivier01A <oliviermasengesho590@gmail.com>
2025-09-15 16:42:58 +0200

    Update index.tsx

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -15,3 +15,14 @@ export default function Home() {
     </>
   );
 }
+
+["const Home: React.FC = () => {"]
+const Home: React.FC = () => {
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+    </main>
+  );
+};
+
+export default Home;
